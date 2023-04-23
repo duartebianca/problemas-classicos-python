447889aa9667f9976bc89510ff494045ebddee6f
duartebianca <82621326+duartebianca@users.noreply.github.com>
2023-04-23 11:52:00 -0300

    Create pi.py

---
+++ Problemas pequenos/pi.py
@@ -0,0 +1,14 @@
+def calculate_pi(n_terms: int) -> float:
+  #Using Leibniz's formula.
+  #sum from 0 to n_terms of ( ((-1)^{n}) / (2n+1)).
+  numerator: float = 4.0
+  denominator: float = 1.0
+  operation: float = 1.0
+  pi: float = 0.0
+  for _ in range(n_terms):
+    pi += operation * (numerator/denominator)
+    denominator += 2.0
+    operation *= -1.0
+  return pi
+
+print(calculate_pi(100000))
